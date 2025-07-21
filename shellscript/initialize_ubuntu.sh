7d69132a84c0fabea9f20d4f17208dc9f33cbadb
rehuony <187857575+rehuony@users.noreply.github.com>
2025-07-21 16:08:15 +0000

    refactor: update initialize_ubuntu.sh to new template

--- shellscript/initialize_ubuntu.sh
+++ shellscript/initialize_ubuntu.sh
@@ -17,9 +17,7 @@ trap remove_temp_directory EXIT
 
 remove_temp_directory() {
   if [[ -n "${TEMPDIRECTORY:-}" && -e "${TEMPDIRECTORY}" ]]; then
-    # Exit temporary working directory
     [[ "$(pwd)" =~ ^"${TEMPDIRECTORY}" ]] && popd &>/dev/null
-    # Delete all temporary files
     rm -rf "${TEMPDIRECTORY}"
   fi
 }
@@ -30,67 +28,161 @@ TEMPDIRECTORY=$(mktemp -dt rehuony_directory_XXXXXX 2>/dev/null) || {
 }
 
 pushd "${TEMPDIRECTORY}" &>/dev/null || {
-  printf "\e[38;2;215;0;0mError: failed to enter temporary directory\e[0m\n"
+  printf "\e[38;2;215;0;0mError: failed to pushd temporary directory\e[0m\n"
   exit 1
 }
 
 # Check whether the execution user is root
-check_is_root() {
-  if [[ $(id -u) != 0 ]]; then
-    printf "\e[38;2;215;0;0mError: please run the script with root permissions\e[0m\n"
+check_permission() {
+  printf "\e[2mCurrent user is: ${USER}\e[0m\n"
+
+  if [[ "${EUID}" != 0 ]]; then
+    printf "\e[38;2;215;0;0mError: please run the script with root\e[0m\n"
     exit 1
   fi
 }
 
-# Check whether the instructions used in the current script exist
-check_command_dependencies() {
-  local lacking_packages command_dependency package_dependency
+# Check the environment of the current script
+check_environment() {
+  [[ -f "/etc/os-release" ]] && source /etc/os-release
 
-  lacking_packages=()
+  os_name=$(echo -ne "${NAME}" | awk '{print tolower($1)}')
+  os_type=$(echo -ne "$(uname -s)" | awk '{print tolower($1)}')
+
+  case "${os_name}" in
+    arch)
+      os_arch=$(uname -m)
+      package_suffix=".zst"
+      package_manager="pacman -S --noconfirm"
+      package_installer="pacman -U --noconfirm"
+      ;;
+    openwrt)
+      os_arch=$(uname -m)
+      package_suffix=".ipk"
+      package_manager="opkg install"
+      package_installer="opkg install"
+      ;;
+    ubuntu | debian)
+      os_arch=$(dpkg --print-architecture)
+      package_suffix=".deb"
+      package_manager="apt install -y"
+      package_installer="dpkg -i"
+      ;;
+    red | centos | fedora)
+      os_arch=$(uname -m)
+      package_suffix=".rpm"
+      package_manager="dnf install -y"
+      package_installer="rpm -i"
+      ;;
+    *)
+      printf "\e[38;2;215;0;0mError: unsupported system for ${os_name}\e[0m\n"
+      exit 1
+      ;;
+  esac
+
+  printf "\e[2mCurrent system is: ${os_arch}_${os_name}_${os_type}\e[0m\n"
+}
+
+# Check whether the instructions used in the current script exist
+check_dependencies() {
+  local command_dependency package_dependency
+
   command_dependency=('sed' 'curl')
   package_dependency=('sed' 'curl')
 
-  for i in "${!command_dependency[@]}"; do
-    if !(type -t "${command_dependency[i]}" &> /dev/null); then
-      lacking_packages+=(${package_dependency[i]})
+  printf "\e[2mChecking command dependencies now ...\e[0m\n"
+
+  for index in "${!command_dependency[@]}"; do
+    printf "\e[4C\e[2m${command_dependency[index]} - "
+
+    if type -t "${command_dependency[index]}" &>/dev/null; then
+      printf "installed\e[0m\n"
+    else
+      printf "not installed\e[0m\n"
+      printf "\e[8C\e[2m${package_manager} ${package_dependency[index]} ... "
+
+      if sh -c "${package_manager} ${package_dependency[index]}" &>/dev/null; then
+        printf "done\e[0m\n"
+      else
+        printf "error\e[0m\n"
+        printf "\e[38;2;215;0;0mError: please run the command manually\e[0m\n"
+        exit 1
+      fi
     fi
   done
+}
 
-  if ((${#lacking_packages[@]} != 0)); then
-    printf "\e[38;2;215;0;0mError: command not found, please execute the following command first\n"
-    printf "\e[38;2;128;128;128msudo apt update && sudo apt install -y %s\e[0m\n" "${lacking_packages[*]}"
-    exit 1
+# Load external script resources
+source_external_scripts() {
+  local script_file lacking_packages external_script_links
+
+  lacking_packages=()
+  external_script_links=(
+    'https://cdn.jsdelivr.net/gh/rehuony/resource@main/shellscript/library/message.lib.sh'
+  )
+
+  printf "\e[2mLoading external scripts now ...\e[0m\n"
+
+  for link in "${external_script_links[@]}"; do
+    printf "\e[4C\e[2mloading ${link} - "
+
+    script_file=$(mktemp -p "${TEMPDIRECTORY}" -t script_XXXXXX.sh 2>/dev/null) || {
+      printf "error\e[0m\n"
+      printf "\e[38;2;215;0;0mError: failed to create temporary file\e[0m\n"
+      exit 1
+    }
+
+    curl -fsSL "${link}" -o "${script_file}" 2>/dev/null || {
+      printf "error\e[0m\n"
+      printf "\e[38;2;215;0;0mError: failed to download external script\e[0m\n"
+      exit 1
+    }
+
+    source "${script_file}"
+
+    for index in "${!lib_command_dependency[@]}"; do
+      type -t "${lib_command_dependency[index]}" &>/dev/null && continue
+
+      local is_exist="false"
+
+      for package in "${lacking_packages[@]}"; do
+        if [[ "${package}" == "${lib_package_dependency[index]}" ]]; then
+          is_exist="true"
+          break
+        fi
+      done
+
+      if [[ "${is_exist}" == "false" ]]; then
+        lacking_packages+=("${lib_package_dependency[index]}")
+      fi
+    done
+
+    printf "done\e[0m\n"
+  done
+
+  printf "\e[2mInstalling external command dependencies ...\e[0m\n"
+
+  if [[ ${#lacking_packages[@]} != 0 ]]; then
+    printf "\e[4C\e[2m${package_manager} ${lacking_packages[*]} ... "
+
+    if sh -c "${package_manager} ${lacking_packages[*]}" &>/dev/null; then
+      printf "done\e[0m\n"
+    else
+      printf "error\e[0m\n"
+      printf "\e[38;2;215;0;0mError: please run the installation command manually\e[0m\n"
+      exit 1
+    fi
   fi
 }
 
-# Load external script resources
-load_external_scripts() {
-  local script_file external_script_links
-
-  external_script_links=(
-    'https://cdn.jsdelivr.net/gh/rehuony/resource@main/shellscript/library/message.sh'
-  )
-
-  for link in "${external_script_links[@]}"; do
-    script_file=$(mktemp -p "${TEMPDIRECTORY}" -t script_XXXXXX.sh 2>/dev/null) || {
-      printf "\e[38;2;215;0;0mError: failed to create temporary file for %s\e[0m\n" "${link}"
-      exit 1
-    }
-    if curl -fsSL "${link}" -o "${script_file}" 2>/dev/null; then
-      source "${script_file}"
-    else
-      printf "\e[38;2;215;0;0mError: failed to download external script: %s\e[0m\n" "${link}"
-      exit 1
-    fi
-  done
-}
-
 # Check whether the execution user is root
-check_is_root
+check_permission
+# Check the environment of the current script
+check_environment
 # Check whether the instructions used in the current script exist
-check_command_dependencies
-# Load external script resources
-load_external_scripts
+check_dependencies
+# Source external script resources
+source_external_scripts
 
 install_content() {
   local mode owner group content destination tempfile
@@ -112,7 +204,7 @@ install_content() {
     return 1
   fi
 
-  show_text "${sgr_faint}install content for ${destination} - "
+  show_text "${sgr_faint}Installing content for ${destination} - "
 
   if [[ -e "${destination}" ]]; then
     show_text "${foreground_color_yellow}"
@@ -143,7 +235,7 @@ remove_content() {
     return 1
   fi
 
-  show_text "${sgr_faint}remove content for ${destination} - "
+  show_text "${sgr_faint}Removing content for ${destination} - "
 
   if ! [[ -e "${destination}" ]]; then
     show_text "${foreground_color_yellow}not exist${sgr_reset}\n"
